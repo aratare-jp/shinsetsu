d841a021119401a78a859f689a8499051f486cf0
Rex Truong <rextruong@pm.me>
2021-01-09 21:08:23 +0700

    Removed blueprint and readded elasticUI. Everything seems to be OK now.

--- src/cljs/harpocrates/core.cljs
+++ src/cljs/harpocrates/core.cljs
@@ -5,11 +5,14 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
     [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
-    ["@blueprintjs/core" :refer [Button]]))
+    [tick.alpha.api :as tick]
+    [cljsjs.moment]
+    ["@elastic/eui" :refer [EuiButton EuiFormRow EuiDatePicker]]))
 
 (defonce app (app/fulcro-app))
 (def dark-mode? (atom false))
 (def person (atom {:person/name "Joe" :person/age 22}))
+(def startDate (js/moment))
 
 (defsc Person [this {:person/keys [name age]}]
   (dom/div
@@ -17,13 +20,17 @@
     (dom/p "Age: " age)))
 
 (def ui-person (comp/factory Person))
-(def ui-button (interop/react-factory Button))
+(def ui-button (interop/react-factory EuiButton))
+(def ui-form-row (interop/react-factory EuiFormRow))
+(def ui-date-picker (interop/react-factory EuiDatePicker))
 
 (defsc Root [this props]
-  (dom/div
-    (if @dark-mode? :.bp3-dark)
-    (ui-person @person)
-    (ui-button {:icon "refresh" :onClick #(swap! person (fn [value] (assoc value :person/name "Jane")))} "Dark mode")))
+  (dom/div :.bp3-dark
+           (ui-person @person)
+           (ui-button {:onClick #(js/console.log "Hello")} "test")
+           (ui-form-row {:label "Select a date"}
+                        (ui-date-picker {:selected startDate
+                                         :onChange #(js/console.log %)}))))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
