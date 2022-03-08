e8c80c250860639a8149771d42db29bcd20d36c9
Rei <58905307+aratare-jp@users.noreply.github.com>
2022-03-08 17:43:37 +0700

    Added migratus for db migration.

---
+++ migratus.clj
@@ -0,0 +1,7 @@
+{:store         :database
+ :migration-dir "migrations"
+ :db            {:classname   "org.postgresql.Driver"
+                 :subprotocol "postgresql"
+                 :subname     "//localhost:5432/shinsetsu"
+                 :user        "shinsetsudev"
+                 :password    "shinsetsu"}}
