07e208d477b3ae8460db9cd4d44aa50e1ad8f7da
Rei <58905307+aratare-jp@users.noreply.github.com>
2022-06-12 15:20:37 +0700

    Testing deployment.

--- build.clj
+++ build.clj
@@ -3,11 +3,22 @@
             [org.corfield.build :as bb]))
 
 (def lib 'com.github.aratare-jp/shinsetsu)
-;; if you want a version of MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-name (str "target/shinsetsu-" version "-standalone.jar"))
+
+(defn pom
+  [_]
+  (b/write-pom {:target   "."
+                :lib      lib
+                :version  version
+                :basis    basis
+                :src-dirs ["src/clj" "src/cljc"]}))
+
 (defn uber
   [_]
-  (bb/uber {:uber-file "target/shinsetsu-standalone.jar"
+  (bb/uber {:uber-file jar-name
             :version   version
             :lib       lib
             :main      'shinsetsu.server}))
