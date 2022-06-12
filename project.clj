c90fc14b9dcde7b602ab4df43fd643cc7184b4ba
Rei <58905307+aratare-jp@users.noreply.github.com>
2022-06-12 20:28:06 +0700

    Testing deployment

--- project.clj
+++ project.clj
@@ -1,2 +1,2 @@
 ; Empty file to play nice with heroku's Clojure buildpack.  This shouldn't be needed once https://github.com/heroku/heroku-buildpack-clojure/issues/75 is fixed.
-{:min-lein-version "2.0.0"}
+
